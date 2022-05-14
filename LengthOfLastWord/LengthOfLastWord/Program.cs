3f93d6ef95f6b7a0ae35be2171f15614d094e6ed
Maharoz <mahruj66@gmail.com>
2022-05-15 00:56:58 +0600

    Plast word solved

---
+++ LengthOfLastWord/LengthOfLastWord/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace LengthOfLastWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            count("   fly me   to   the moon  ");
+        }
+
+        public static void count(string s)
+        {
+            //string lastWord = s.Split(' ').Last();
+
+            string[] parts = s.Split(' ');
+            parts = parts.Where(x => x != string.Empty).ToArray();
+            string lastWord = parts[parts.Length - 1];
+         
+            int count = lastWord.ToCharArray().Count();
+        }
+    }
+
+    
+}
