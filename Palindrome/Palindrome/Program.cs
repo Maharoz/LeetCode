58cc1589afb318ef7aad91471aab745d70a5d496
Maharoz <mahruj66@gmail.com>
2022-05-14 23:53:39 +0600

    Palindrome solved

---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            palindrome c = new palindrome();
+           bool x= c.check(-121);
+        }
+
+        class palindrome {
+            public int reminder,sum=0;
+            public int temp;
+            public bool check(int number)
+            {
+                temp = number;
+
+                if (number < 0)
+                {
+                    return false;
+                }
+                while (number > 0)
+                {
+                    reminder = number % 10;
+
+                    sum = (sum * 10) + reminder;
+
+                    number = number / 10;
+                }
+
+                if (temp == sum)
+                {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+    }
+}
