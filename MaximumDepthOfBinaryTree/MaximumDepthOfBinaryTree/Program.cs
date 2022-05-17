8d098658c5f48b9628c32f903906edaaf1ebcce0
Maharoz <mahruj66@gmail.com>
2022-05-17 22:22:37 +0600

    binary tree preorder traversal

--- MaximumDepthOfBinaryTree/MaximumDepthOfBinaryTree/Program.cs
+++ MaximumDepthOfBinaryTree/MaximumDepthOfBinaryTree/Program.cs
@@ -8,5 +8,33 @@ namespace MaximumDepthOfBinaryTree
         {
             Console.WriteLine("Hello World!");
         }
+
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            if (root.left == null && root.right == null)
+            {
+                return 1;
+            }
+
+            return 1 + Math.Max(MaxDepth(root.left), MaxDepth(root.right));
+        }
     }
+
+     public class TreeNode
+    {
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+      public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+       {
+           this.val = val;
+           this.left = left;
+           this.right = right;
+                }
+  }
+ 
 }
