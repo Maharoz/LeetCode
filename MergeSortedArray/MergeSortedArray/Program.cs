32660e754ddc5a9ebfa54e107b45a899f99d5e55
Maharoz <mahruj66@gmail.com>
2022-06-10 00:36:45 +0600

    merge to sorted list

---
+++ MergeSortedArray/MergeSortedArray/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MergeSortedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1 = { 0 };
+            int m = 0,ml=m;
+            int[] nums2 = {1 };
+            int n =1,nl=n;
+
+            if (n == 0)
+            {
+                return;
+            }
+
+            if (m == 0)
+            {
+                nums1 = nums2;
+                return;
+            }
+
+            while (ml<m+n)
+            {
+                nums1[ml] = nums2[ nl-n];
+                ml++;
+                nl++;
+            }
+
+            Array.Sort(nums1);
+
+        }
+    }
+}
