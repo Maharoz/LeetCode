6e54d6b2bb49efe0b2bd99b6884217240c38ec4c
Maharoz <mahruj66@gmail.com>
2022-08-21 23:30:22 +0600

    asteriod problem added

---
+++ Asteroid Collision/Asteroid Collision/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] x = {10,2,-5 };
+AsteroidCollision(x);
+
+ static int[] AsteroidCollision(int[] asteriods)
+{
+    if(asteriods == null|| asteriods.Length == 0)
+    {
+        return null;
+    }
+
+    var stack = new Stack<int>();
+    stack.Push(asteriods[0]);
+
+
+    for(int i =1; i < asteriods.Length; i++)
+    {
+        //while (true)
+        //{
+            if (stack.Count > 0 && stack.Peek() > 0 && asteriods[i] < 0){
+                if (Math.Abs(stack.Peek()) == Math.Abs(asteriods[i]))
+                {
+                    stack.Pop();
+                    break;
+                }
+                else if (Math.Abs(stack.Peek()) < Math.Abs(asteriods[i]))
+                {
+                    stack.Pop();
+
+                }
+                else break;
+            }
+            else
+            {
+                stack.Push(asteriods[i]);
+                break;
+            }
+      //  }
+    }
+    return stack.Reverse().ToArray();
+}
---
+++ ReverseStingII/ReverseStingII/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace ReverseStingII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string s = "abcdefg";
+            int k = 2;
+            string input = s.Substring(0, k);
+            string output= rev(input);
+            output = output + s.Substring(k,s.Length - k);
+
+
+        }
+
+        public static string rev(string s) { 
+
+            StringBuilder rev = new StringBuilder();
+
+            for (int i = s.Length; i > 0; i--)
+            {
+                rev.Append(s.ToCharArray()[i - 1]);
+            }
+            return rev.ToString();
+        }
+    }
+}
