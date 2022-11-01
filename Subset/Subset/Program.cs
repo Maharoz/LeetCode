f82507154b264a20855169c663a81037515a78c7
Maharoz <mahruj66@gmail.com>
2022-11-02 00:55:32 +0600

    subsets

---
+++ Subset/Subset/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Subset
+{
+
+    //https://leetcode.com/problems/subsets/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3 };
+            IList<IList<int>> resultList = new List<IList<int>>();
+            backTrack(resultList, new List<int>(), nums, 0);
+            Console.WriteLine(resultList);
+        }
+
+        private static void backTrack(IList<IList<int>> resultSets,List<int> tempSets, int[] nums,int start)
+        {
+            resultSets.Add(new List<int>(tempSets));
+
+            for (int i = start; i < nums.Length; i++)
+            {
+                tempSets.Add(nums[i]);
+
+                backTrack(resultSets, tempSets, nums, i + 1);
+                tempSets.Remove(tempSets.Count() - 1);
+            }
+        }
+    }
+}
