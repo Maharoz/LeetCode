52063baa8e83c4367fb1fa75965776d8d3bae805
Maharoz <mahruj66@gmail.com>
2022-11-02 20:56:14 +0600

    valid anagram

---
+++ ValidAnagram/ValidAnagram/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ValidAnagram
+{
+
+    //https://leetcode.com/problems/valid-anagram/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IsAnagram("anagram", "nagaram");
+        }
+
+        static bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length)
+                return false;
+
+            char[] source = s.ToCharArray(), target = t.ToCharArray();
+
+            Array.Sort(source);
+            Array.Sort(target);
+
+            for (int i = 0; i <= source.Length - 1; i++)
+                if (source[i] != target[i])
+                    return false;
+
+            return true;
+        }
+    }
+}
