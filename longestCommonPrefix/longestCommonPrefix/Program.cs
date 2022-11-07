d227273a49fd9b63ccda33cd670fb10270faa9dd
Maharoz <mahruj66@gmail.com>
2022-11-08 01:25:54 +0600

    longest common prefix

---
+++ longestCommonPrefix/longestCommonPrefix/Program.cs
@@ -0,0 +1,16 @@
+﻿//https://leetcode.com/problems/longest-common-prefix/
+
+
+static string LongestCommonPrefix(string[] strs)
+{
+    string s = strs[0];
+
+  for(int i = 1; i < strs.Length; i++)
+    {
+        while (strs[i].IndexOf(s) != 0)
+        {
+            s=s.Substring(0,s.Length-1);    
+        }
+    }
+  return s;
+}
