2b2ea66b9ece6212995e51faefab14c7febe4273
Maharoz <mahruj66@gmail.com>
2022-11-12 22:55:44 +0600

    find-all-duplicates-in-an-array

---
+++ FindAllDuplicateNumberInAnArray/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace FindAllDuplicateNumberInAnArray
+{
+    //https://leetcode.com/problems/find-all-duplicates-in-an-array/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 10,2,5,10,9,1,1,4,3,7};
+            // Console.WriteLine("Hello, World!");
+            FindDuplicates(a);
+        }
+
+        static IList<int> FindDuplicates(int[] nums)
+        {
+            IList<int> result = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int val = Math.Abs(nums[i]) - 1;
+                if (nums[val] > 0)
+                    nums[val] = -nums[val];
+                else
+                {
+                    result.Add(val+1);
+                }
+            }
+
+            return result;
+        }
+    }
+}
