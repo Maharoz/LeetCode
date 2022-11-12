e7b501891ba87a0231e0c1680d7ceba988bd9e18
Maharoz <mahruj66@gmail.com>
2022-11-12 18:12:40 +0600

    generate parenthesis

---
+++ GenerateParanthesis/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace GenerateParanthesis
+{
+
+    //https://leetcode.com/problems/generate-parentheses/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GenerateParenthesis(3);
+        }
+
+
+        static IList<string> GenerateParenthesis(int n)
+        {
+            IList<string> strings = new List<string>();
+            findAll("(", 1, 0, strings, n);
+            return strings;
+        }
+
+        public static void findAll(string current, int o,int c,IList<string> result,int n)
+        {
+            if (current.Length == 2 * n)
+            {
+                result.Add(current);
+                return;
+            }
+            if (o < n)
+            {
+                findAll(current +"(", o + 1, c, result, n);
+
+            }
+            if (c < o)
+            {
+                findAll(current + ")", o, c+1, result, n);
+            }
+        }
+    }
+}
