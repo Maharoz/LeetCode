f5641b0c50c349e2b424024727a553ddfd9d1b95
Maharoz <mahruj66@gmail.com>
2022-11-17 00:05:14 +0600

    Ransom note solved

---
+++ RansomNote/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace RansomNote
+{
+
+    //https://leetcode.com/problems/ransom-note/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static bool CanConstruct(string ransomNote, string magazine)
+        {
+
+            var charCount = new int[256];
+            foreach (char c in magazine)
+            {
+                charCount[c]++;
+            }
+
+            foreach(var c in ransomNote)
+            {
+                charCount[c]--;
+                if(charCount[c] < 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
