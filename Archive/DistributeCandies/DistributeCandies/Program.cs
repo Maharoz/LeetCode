28465a13296c9db7c0c3966d4838753b83d675a4
Maharoz <mahruj66@gmail.com>
2023-06-09 21:12:11 +0600

    archives old code

---
+++ Archive/DistributeCandies/DistributeCandies/Program.cs
@@ -0,0 +1,16 @@
+﻿
+//https://leetcode.com/problems/distribute-candies/
+using System;
+
+int[] n = { 2, 5, 9, 3, 6 };
+
+static int DistributeCandies(int[] candyType)
+{
+    int x = candyType.Distinct().Count();
+
+    if(x > candyType.Length / 2)
+    {
+        return candyType.Length / 2;
+    }
+    return x;
+}
