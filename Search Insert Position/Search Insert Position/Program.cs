b8ae3269c6c4fde807b16b1c839d1c77da20e751
Maharoz <mahruj66@gmail.com>
2023-06-22 17:38:34 +0600

    Two sum solved

---
+++ Search Insert Position/Search Insert Position/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Search_Insert_Position
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+
+	public class Solution
+	{
+		public int SearchInsert(int[] nums, int target)
+		{
+			for(int i = nums[0];i< nums.Length-1;i++)
+			{
+				if(target=)
+			}
+		}
+	}
+}
