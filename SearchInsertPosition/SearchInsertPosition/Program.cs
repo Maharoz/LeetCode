d12186449913d163294f4a1c2b6544132da468f8
Maharoz <mahruj66@gmail.com>
2023-07-09 01:35:12 +0600

    valid anagram

---
+++ SearchInsertPosition/SearchInsertPosition/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace SearchInsertPosition
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//Console.WriteLine("Hello, World!");
+			int[] a = { 1};
+			int x = SearchInsert(a, 0);
+		}
+		public static int SearchInsert(int[] nums, int target)
+		{
+
+			int low = 0;
+			int high = nums.Length - 1;
+
+			while (low <= high)
+			{
+				int mid = low + (high-low) / 2;
+				if (nums[mid] == target)
+				{
+					return mid;
+				}
+				else if (nums[mid] > target)
+				{
+					high = mid - 1;
+				}
+				else
+				{
+					low = mid + 1;
+				}
+
+
+			}
+			return low;
+		}
+	}
+}
---
+++ ValidAnagram/ValidAnagram/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ValidAnagram
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+			IsAnagram("rat", "car");
+		}
+
+		public static bool IsAnagram(string s, string t)
+		{
+			bool result = true;
+
+
+			s= s.ToLower();
+			t=t.ToLower();
+
+
+			s = s.Replace(" ", "");
+			t = t.Replace(" ", "");
+
+			int[] x = new int[26];
+			
+		  for(int i=0;i<s.Length;i++)
+			{
+				x[s[i] - 'a']++;
+			}
+
+			for (int i = 0; i < t.Length; i++)
+			{
+				x[t[i] - 'a']--;
+			}
+
+			foreach (int i in x)
+			{
+				if(i != 0)
+				{
+					return false;
+				}
+			}
+
+			return result;
+		}
+	}
+}
