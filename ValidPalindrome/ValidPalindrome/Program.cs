01b0f5b24bd5a951bd98bc73dc5849ff53ccea69
Maharoz <mahruj66@gmail.com>
2023-07-09 14:48:34 +0600

    is subsequence

--- ValidPalindrome/ValidPalindrome/Program.cs
+++ ValidPalindrome/ValidPalindrome/Program.cs
@@ -6,5 +6,33 @@
 		{
 			Console.WriteLine("Hello, World!");
 		}
+
+		public static bool IsPalindrome(string s)
+		{
+			string fixed_string = string.Empty;
+
+			foreach (char c in s.ToCharArray())
+			{
+				if (char.IsLetter(c) || char.IsDigit(c))
+				{
+					fixed_string += c;
+				}
+			}
+
+			fixed_string= fixed_string.ToLower();
+			char[] fixed_string_char= fixed_string.ToCharArray();
+			int startPoint = 0;
+			int endPoint = fixed_string.Length-1;
+
+			while (startPoint < endPoint)
+			{
+				if (fixed_string_char[startPoint]!= fixed_string_char[endPoint])
+				{
+					return false;
+				}
+				startPoint++;
+			}
+			return true;
+		}
 	}
 }
