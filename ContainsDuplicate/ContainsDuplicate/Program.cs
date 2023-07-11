dc04c832fa240753cb3a9864a81ce4ee74776dbd
Maharoz <mahruj66@gmail.com>
2023-07-12 02:05:18 +0600

    contain duplicate solved

---
+++ ContainsDuplicate/ContainsDuplicate/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ContainsDuplicate
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+
+		public bool ContainsDuplicate(int[] nums)
+		{
+			int distict = nums.Distinct().Count();
+			if(nums.Length > distict) return true;
+			return false;
+		}
+
+	}
+}
--- JewelAndStone/JewelAndStone/Program.cs
+++ JewelAndStone/JewelAndStone/Program.cs
@@ -6,5 +6,20 @@
 		{
 			Console.WriteLine("Hello, World!");
 		}
+		public int NumJewelsInStones(string jewels, string stones)
+		{
+			int res = 0;
+			List<char> jewelArr = jewels.ToCharArray().ToList();
+			List<char> stoneArr= stones.ToCharArray().ToList();	
+			foreach(char c in stoneArr)
+			{
+				if (jewelArr.Contains(c))
+				{
+					res++;
+				}
+			}
+
+			return res;
+		}
 	}
 }
