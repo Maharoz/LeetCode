cc4aee266bdb985b663dab7f79fbfc2abf4b093f
Maharoz <mahruj66@gmail.com>
2023-07-13 12:46:57 +0600

    sort color solved

---
+++ MergeInterval/MergeInterval/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+
+namespace MergeInterval
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+			
+		}
+
+		public static int[][] Merge(int[][] intervals)
+		{
+			List<int[]> output = new List<int[]>();
+
+			Array.Sort(intervals, (a, b) => { return a[0] - b[0]; });
+
+			output.Add(intervals[0]);
+
+			for (int i = 1; i < intervals.Length; i++)
+			{
+				if (output[output.Count - 1][1] >= intervals[i][0])
+				{
+					if (output[output.Count - 1][1] <= intervals[i][1])
+						output[output.Count - 1][1] = intervals[i][1];
+				}
+				else output.Add(intervals[i]);
+			}
+
+			return output.ToArray();
+
+
+		}
+	}
+}
---
+++ SortColor/SortColor/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace SortColor
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+			int[] nums = { 2,0,2,1,1,0,1,0,1,0 };
+			SortColors(nums);
+		}
+
+		public static void SortColors(int[] nums)
+		{
+			int start = 0;
+			int mid = 0;
+			int end = nums.Length - 1;
+
+			while (mid <= end)
+			{
+				switch(nums[mid])
+				{
+					case 0:
+						swap(nums, start, mid);
+						mid++;
+						start++;
+						break;
+					case 1:
+						mid++;
+						break;
+					case 2:
+						swap(nums, mid, end);
+						end--;
+						break;
+
+				}
+			}
+		}
+
+		private static void swap(int[] arr,int pos1,int pos2)
+		{
+			int temp = arr[pos1];
+			arr[pos1] = arr[pos2];
+			arr[pos2] = temp;
+		}
+
+
+	}
+}
