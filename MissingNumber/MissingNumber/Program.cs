2b24d6ed9f83325f20a5e8bcd03a80a3a729213d
Maharoz <mahruj66@gmail.com>
2023-07-13 12:58:20 +0600

    missing number solved solved

---
+++ MissingNumber/MissingNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace MissingNumber
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+			int[] x = { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
+			MissingNumber(x);
+		}
+
+		public static int MissingNumber(int[] nums)
+		{
+			int res = 0;
+			int sum = nums.Length * (nums.Length + 1) / 2;
+			res = sum - nums.Sum();
+
+			return res;
+		}
+	}
+}
