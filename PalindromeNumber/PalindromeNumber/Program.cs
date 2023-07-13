0e2e2eeebf2ce4d5b11bc9067b7a6e6e326349dc
Maharoz <mahruj66@gmail.com>
2023-07-13 14:20:11 +0600

    Palindrome solved

---
+++ PalindromeNumber/PalindromeNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PalindromeNumber
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+			IsPalindrome(121);
+		}
+
+		public static bool IsPalindrome(int x)
+		{
+			if(x < 0)
+			{
+				return false;
+			}
+			int r = 0;
+			int c=x;
+			while (c > 0)
+			{
+				r = r * 10 + c % 10;
+				c /= 10;
+			}
+			return r == x;
+		}
+	}
+}
