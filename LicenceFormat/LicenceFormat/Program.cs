085af8b136ff55154e9269e01946f0e586aa6646
Maharoz <mahruj66@gmail.com>
2023-07-15 14:23:24 +0600

    formatting the licence

---
+++ LicenceFormat/LicenceFormat/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace LicenceFormat
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+			LicenseKeyFormatting("---", 3);
+		}
+
+		public static string LicenseKeyFormatting(string s, int k)
+		{
+			s= s.Replace("-", "").ToUpper();
+		    char[] sa= s.ToCharArray();
+			StringBuilder sb = new StringBuilder();
+			int clusterSize = sa.Length/k;
+			int firstCluster = sa.Length % k;
+
+			for(int i = 0;i< firstCluster; i++)
+			{
+				sb.Append(sa[i]);
+			}
+			if(firstCluster != 0) { sb.Append("-"); }
+			
+			for (int i = firstCluster; i < sa.Length; i = i + k)
+			{
+				for(int j = i; j < i + k; j++)
+				{
+					sb.Append(sa[j]);
+				}
+				
+				sb.Append("-"); 
+			}
+			string ss = sb.ToString();
+			return ss.Remove(sb.Length-1,1);
+
+		}
+	}
+}
