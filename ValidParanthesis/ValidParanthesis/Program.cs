38a2a5c9441e8072e5431914c534337706eda96e
Maharoz <mahruj66@gmail.com>
2023-07-18 11:55:49 +0600

    valid paranthesis implemented

---
+++ ValidParanthesis/ValidParanthesis/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+
+namespace ValidParanthesis
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//Console.WriteLine("Hello, World!");
+			bool res = IsValid("]");
+		}
+
+		public static bool IsValid(string s)
+		{
+			Stack<char> stack = new Stack<char>();
+			char[] chars = s.ToCharArray();
+			foreach (char c in chars)
+			{
+				if (c == '(' || c == '{' || c == '[')
+				{
+					stack.Push(c);
+				}
+				else
+				{
+					if ( c == ')')
+					{
+						if (stack.Count > 0 &&  stack.Peek() == '(')
+						{
+							stack.Pop();
+						}
+						else
+						{
+							return false;
+						}
+					}else if ( c == '}')
+					{
+						if (stack.Count > 0 &&  stack.Peek() == '{')
+						{
+							stack.Pop();
+						}
+						else
+						{
+							return false;
+						}
+					}
+					else if ( c == ']')
+					{
+						if (stack.Count > 0 && stack.Peek() == '[')
+						{
+							stack.Pop();
+						}
+						else
+						{
+							return false;
+						}
+					}
+				}
+			}
+			return stack.Count==0?true:false;
+		}
+	}
+}
