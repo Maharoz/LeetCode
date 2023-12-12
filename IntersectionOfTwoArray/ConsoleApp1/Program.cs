73db6c3a82d3362fb97b072057bb72ab8d2309b5
Maharoz <mahruj66@gmail.com>
2023-12-12 22:25:50 +0600

    Intersection of Two array

---
+++ IntersectionOfTwoArray/ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+
+//https://leetcode.com/problems/intersection-of-two-arrays/submissions/
+static int[] Intersection(int[] nums1, int[] nums2)
+{
+    int[] x = nums1.Intersect(nums2).ToArray();
+    return x;
+}
---
+++ RomanToInteger/RomanToInteger/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace RomanToInteger
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			RomanToInt("MCMXCIV");
+		}
+
+		public static int RomanToInt(string s)
+		{
+			Dictionary<char,int> map = new Dictionary<char,int>();
+			map.Add('I', 1);
+			map.Add('V', 5);
+			map.Add('X', 10);
+			map.Add('L', 50);
+			map.Add('C', 100);
+			map.Add('D', 500);
+			map.Add('M', 1000);	
+			int result = 0;
+			char[] chars = s.ToCharArray();
+
+			foreach (char c in chars)
+			{
+				int val= map[c];
+				result = result + val;
+			}
+			return result;
+
+		}
+		
+	}
+}
