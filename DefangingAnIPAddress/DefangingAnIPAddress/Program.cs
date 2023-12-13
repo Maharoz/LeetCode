5e1be635d4f3ef5d5e2277dacc3d3f69787d19eb
Maharoz <mahruj66@gmail.com>
2023-12-13 12:50:05 +0600

    defanging an IP

---
+++ DefangingAnIPAddress/DefangingAnIPAddress/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace DefangingAnIPAddress
+{
+    //https://leetcode.com/problems/defanging-an-ip-address/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DefangIPaddr("1.1.1.1");
+        }
+
+        public static string DefangIPaddr(string address)
+        {
+            string result =address.Replace(".", "[.]");
+            return result;
+        }
+    }
+}
