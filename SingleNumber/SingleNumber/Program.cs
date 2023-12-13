ca339b6df4124269fd966f1fbf29a9ff5706d344
Maharoz <mahruj66@gmail.com>
2023-12-13 11:55:32 +0600

    single number leetcode

---
+++ SingleNumber/SingleNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace SingleNumber
+{
+
+    //https://leetcode.com/problems/single-number/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = { 1, 2, 2 };
+            SingleNumber(x);
+        }
+
+        public static int SingleNumber(int[] nums)
+        {
+            
+            return nums.GroupBy(x=>x).Where(g=>g.Count()==1).Select(g=>g.Key).FirstOrDefault();
+            
+        }
+    }
+}
