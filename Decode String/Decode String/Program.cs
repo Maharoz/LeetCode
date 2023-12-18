d2024ac91193bdb356cbba314b79e6ec32d49a41
Maharoz <mahruj66@gmail.com>
2023-12-19 00:48:55 +0600

    max product difference slved

---
+++ Decode String/Decode String/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+DecodeString("3[a]2[bc]");
+
+string DecodeString(string s)
+{
+   string result = string.Empty;
+   Stack<string> elmentStack = new Stack<string>();
+    Stack<int> multiplierStack = new Stack<int>();
+
+    foreach(char c in s)
+    {
+        if (char.IsDigit(c))
+        {
+            multiplierStack.Push(c);
+        }
+        else
+        {
+            elmentStack.Push(c.ToString());
+        }
+    }
+    return result;
+}
