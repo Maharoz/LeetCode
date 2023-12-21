d52d8d99ef7967233fa4d8976f57a332221b7412
Maharoz <mahruj66@gmail.com>
2023-12-21 18:19:01 +0600

    Remove element

---
+++ RemoveElement/RemoveElement/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace RemoveElement
+{
+
+    //https://leetcode.com/problems/remove-element/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = { 3,2,2,3 };
+            int val = 3;
+            RemoveElement(x, val);
+        }
+
+        public static int RemoveElement(int[] nums, int val)
+        {
+            int result = 0;
+            foreach (var item in nums)
+            {
+                if (item != val)
+                {
+                    nums[result] = item;
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
