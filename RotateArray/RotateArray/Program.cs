f54558c6da3b97adde4e73fcaca23fffb7c14764
Maharoz <mahruj66@gmail.com>
2023-12-23 19:08:37 +0600

    rotate array implemented

---
+++ RotateArray/RotateArray/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace RotateArray
+{
+
+    //https://leetcode.com/problems/rotate-array/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = { 1, 2, 3 };
+            Rotate(x, 4);
+        }
+
+        public static void Rotate(int[] nums, int k)
+        {
+
+
+            k = k % nums.Length;
+            Array.Reverse(nums);
+            Array.Reverse(nums, 0, k);
+            Array.Reverse(nums, k, nums.Length - k);
+
+        }
+    }
+}
