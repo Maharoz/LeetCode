9002f1d2c5bab2ec9f1be3a2389d9363035d6587
Maharoz <mahruj66@gmail.com>
2024-01-21 14:07:38 +0600

    Truncate sentence

---
+++ TruncateSentence/TruncateSentence/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TruncateSentence
+{
+
+    //https://leetcode.com/problems/truncate-sentence/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            TruncateSentence("Hello how are you Contestant", 4);
+        }
+
+
+        public static string TruncateSentence(string s, int k)
+        {
+            string[] sentence = s.Split(" ").Take(k).ToArray();
+            string x = string.Join(" ", sentence);
+            return x;
+        }
+    }
+}
