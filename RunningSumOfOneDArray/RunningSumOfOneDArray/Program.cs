4d0ab15bf23c49b69eed38fb663a6a6cfc5bb69c
Maharoz <mahruj66@gmail.com>
2024-01-21 14:24:30 +0600

    Running sum of 1D Array

---
+++ RunningSumOfOneDArray/RunningSumOfOneDArray/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace RunningSumOfOneDArray
+{
+    //https://leetcode.com/problems/running-sum-of-1d-array/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4 };
+            RunningSum(nums);
+        }
+
+        public static int[] RunningSum(int[] nums)
+        {
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (i != 0)
+                {
+                    nums[i] = nums[i] + nums[i - 1];
+                }
+            }
+            return nums;
+        }
+    }
+}
