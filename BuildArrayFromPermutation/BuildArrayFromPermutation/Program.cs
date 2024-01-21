45147e9cd04a10db5961f06654acd6d6fa25505f
Maharoz <mahruj66@gmail.com>
2024-01-21 13:56:42 +0600

    Buils Array From Permutation

---
+++ BuildArrayFromPermutation/BuildArrayFromPermutation/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace BuildArrayFromPermutation
+{
+    internal class Program
+    {
+        //https://leetcode.com/problems/build-array-from-permutation/
+        static void Main(string[] args)
+        {
+            int[] nums = { 0, 2, 1, 5, 3, 4 };
+            BuildArray(nums);
+        }
+        public static int[] BuildArray(int[] nums)
+        {
+            int[] ints = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                ints[i] = nums[nums[i]];
+            }
+
+            return ints;
+        }
+    }
+}
