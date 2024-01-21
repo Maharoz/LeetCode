44342aff021b71de18e3f87dcaea1f454c50acbb
Maharoz <mahruj66@gmail.com>
2024-01-21 13:50:27 +0600

    Concatenation of array

---
+++ ConCatenationOfArray/ConCatenationOfArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConCatenationOfArray
+{
+
+    //https://leetcode.com/problems/concatenation-of-array/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 1 };
+            GetConcatenation(nums);
+        }
+
+        public static int[] GetConcatenation(int[] nums)
+        {
+            int[] res = new int[nums.Length*2];
+            nums.CopyTo(res, 0);
+            nums.CopyTo(res, nums.Length);
+
+            return res;
+        }
+    }
+}
