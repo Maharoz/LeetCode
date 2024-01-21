86c54d2ee06bcb2487f3c6f90d78b4c73c922724
Maharoz <mahruj66@gmail.com>
2024-01-21 14:14:18 +0600

    number-of-employees-who-met-the-target

---
+++ NumberofEmployeeWhoMetTarget/NumberofEmployeeWhoMetTarget/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace NumberofEmployeeWhoMetTarget
+{
+    //https://leetcode.com/problems/number-of-employees-who-met-the-target/description/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public static int NumberOfEmployeesWhoMetTarget(int[] hours, int target)
+        {
+            Array.Sort(hours);
+            int number = hours.Where(h => h >= target).Count();
+            return number;
+        }
+    }
+}
