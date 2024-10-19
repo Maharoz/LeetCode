afe4df0eb66c2376ac690941b0d62ecc8ab510c5
Maharoz <mahruj66@gmail.com>
2024-10-20 00:37:01 +0600

    Arranging coins

---
+++ Arranging Coins/Arranging Coins/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Arranging_Coins
+{
+	internal class Program
+	{
+		//441. Arranging Coins
+		static void Main(string[] args)
+		{
+			Console.WriteLine(ArrangeCoins(8));
+		}
+
+		public static int ArrangeCoins(int n)
+		{
+			int counter = 1;
+			int remaining = n;
+			while (counter<=remaining)
+			{
+				remaining = remaining - counter;
+				counter++;
+			}
+
+			return counter-1;
+		}
+	}
+}
