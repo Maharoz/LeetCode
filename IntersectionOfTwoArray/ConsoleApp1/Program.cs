da7f5472e9c6d1524567494f7b6c0ce7cfe95a46
Maharoz <mahruj66@gmail.com>
2024-10-19 20:16:00 +0600

    Restarted

--- IntersectionOfTwoArray/ConsoleApp1/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿
-
-
-//https://leetcode.com/problems/intersection-of-two-arrays/submissions/
-static int[] Intersection(int[] nums1, int[] nums2)
-{
-    int[] x = nums1.Intersect(nums2).ToArray();
-    return x;
-}
